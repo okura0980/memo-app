4d74acc1bc9ba59532f9f436d3d7cd5f1443ab9a
okura <okura7982@gmail.com>
2026-04-23 16:46:20 +0900

    third

---
+++ laravel/resources/js/components/SaveButton.vue
@@ -0,0 +1,43 @@
+<script setup> import PlusSvg from './svgs/PlusSvg.vue'
+defineProps({ disabled: Boolean })
+const emit = defineEmits(['click'])
+</script>
+<template>
+    <button :disabled="disabled"
+            class="button"
+            :class="disabled ? 'disabled' : 'active'"
+            @click="emit('click')"
+    >
+        <div class="in"> <PlusSvg />
+            <span>メモを保存</span>
+        </div>
+    </button>
+</template>
+<style scoped>
+.button {
+    width: 100%;
+    margin-top: 16px;
+    padding: 12px;
+    border-radius: 8px;
+    border: none; color: white;
+    font-size: 16px; }
+
+.in {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px; }
+
+.button.disabled {
+    background: rgba(223, 141, 81, 0.6);
+    opacity: 0.6;
+    cursor: not-allowed; }
+
+.button.active {
+    background: #f59e0b;
+    cursor: pointer; }
+
+.button.active:hover {
+    background: #ea8a00;
+}
+</style>
